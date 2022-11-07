adf38dc79be22e3d2f11da759a26ec5b1e8f9827
Genry Leyva <gleyvaglez@gmail.com>
2022-11-07 09:28:28 -0300

    adding test project

---
+++ Utils/Dateutils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WebApp.Utils
+{
+    public static class Dateutils
+    {
+        public static string DateAsString(DateTime date)
+        {
+            return date.ToString();
+        }
+
+        public static long DateAsTimestamp(DateTime date)
+        {
+            return (date.Ticks - 621355968000000000) / 10000000;
+        }
+    }
+}
